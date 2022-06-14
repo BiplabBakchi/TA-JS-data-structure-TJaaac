33732168f05dfd098658a32e527afe488b4e34c4
Biplab Bakchi <biplabbakchi3@gmail.com>
2022-06-14 11:36:27 +0530

    Initial Push

--- block-BJaaco/code/index.js
+++ block-BJaaco/code/index.js
@@ -3,11 +3,33 @@ let numbers = [6, 8, 10, 12, 43, 56, 98];
 let userIds = [1230, 234, 1278, 984, 763, 900];
 
 // 1. Add all the values of numbers and userIds array into the new newly created array named `collection`
-
+let collection = [numbers , userIds];
 // 2. Add all the even numbers from both arrays numbers and userIds into a newly created array named `evenCollection`
+let evenCollection = [];
+for (number of numbers) {
+  if (number % 2 == 0) {
+    evenCollection.push(number)
+  }
+}
 
+for (user of userIds) {
+  if(user % 2 == 0) {
+    evenCollection.push(user)
+  }
+}
 // 3. Add all the odd numbers from both arrays numbers and userIds into a newly created array named `oddCollection`
+let oddCollection = [];
+for (number of numbers) {
+  if (number % 2 != 0) {
+    oddCollection.push(number)
+  }
+}
 
+for (user of userIds) {
+  if(user % 2 != 0) {
+    oddCollection.push(user)
+  }
+}
 /*
   @param means parameter
 
@@ -23,10 +45,13 @@ let userIds = [1230, 234, 1278, 984, 763, 900];
     times(0); // []
     times(5); // ['test', 'test', 'test', 'test', 'test']
 */
-
-function times() {
-  // Your code
+// Your code
+function times(a , b) {
+ for (i = b; i <= a; i++) {
+  return (i)
+ }
 }
+times(5, 'c');
 
 // Uncomment the code below and test the output
 
